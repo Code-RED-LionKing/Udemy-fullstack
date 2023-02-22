7876a5a2dfb6d4c6bf418983d8a29fa44c17191d
yongyong <wy0628j@gmail.com>
2023-02-22 22:01:12 +0900

    javascript add. tictaetoe

---
+++ Tic-Tac-Toe/scripts/config.js
@@ -0,0 +1,23 @@
+function openPlayerConfig() {
+    openPlayerConfigOverlayElement.style.display='block';
+    backdropElement.style.display='block';
+
+
+}
+
+function closePlayerConfig() {
+
+    openPlayerConfigOverlayElement.style.display='none';
+    backdropElement.style.display='none';
+
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername');
+    console.log(enteredPlayername);
+
+
+}
+
