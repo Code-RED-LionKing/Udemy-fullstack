3260e44e4a102bc485b2576af6188a5eabf3c30e
yongyong <wy0628j@gmail.com>
2023-03-08 19:48:10 +0900

    1

--- node_js_start/app.js
+++ node_js_start/app.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 const express = require('express');
 
 const app = express();
@@ -15,7 +18,16 @@ app.get('/', function(request,response) {
 
 app.post('/store-user', function(req, res) {
     const userName = req.body.username;
-    console.log(userName);
+    
+    const filePath = path.join(__dirname, 'data', 'users.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    existingUsers.push(userName);
+
+    fs.writeFileSync(filePath, )
+
     res.send('<h1>Username stored!</h1>');
 
 
