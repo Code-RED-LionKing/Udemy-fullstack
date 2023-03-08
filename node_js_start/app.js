65fbecef8ee2b5d66ab74f8865af26bf9fba01bb
yongyong <wy0628j@gmail.com>
2023-03-08 17:33:55 +0900

    node

--- node_js_start/app.js
+++ node_js_start/app.js
@@ -1,6 +1,30 @@
-const http = require('http')
+const express = require('express');
 
-function handleRequest(request, response) {
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+app.get('/currenttime', function(request, response) {
+    response.send('<h1>' + new Date().toISOString() +'</h1>');
+
+}); // localhost:3000/currenttime
+
+app.get('/', function(request,response) {
+    response.send('<form action="/store-user" method ="POST"><label>Your Name</label><input type= "text" name = "username"><button>Submit</button></form>');
+}); //localhost:3000
+
+app.post('/store-user', function(req, res) {
+    const userName = req.body.username;
+    console.log(userName);
+    res.send('<h1>Username stored!</h1>');
+
+
+});
+
+app.listen(3000);
+
+
+/*function handleRequest(request, response) {
 
    if (request.url === '/currenttime') {
     response.statusCode = 200;
@@ -19,3 +43,4 @@ function handleRequest(request, response) {
 const server = http.createServer(handleRequest);
 
 server.listen(3000);
+*/
